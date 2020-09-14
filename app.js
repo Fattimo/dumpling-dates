70fbccca6b6670da9994b1bbbae793f57b4f6c80
Matt Chen <matt.chen522@gmail.com>
2020-09-14 14:43:47 -0400

    final message organization before regular development

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = new App({
 
 
 // All the room in the world for your code
-import messages from "./assets/messages"
+const messages = require("./assets/messages.json")
 
 app.event('app_home_opened', async ({ event, context }) => {
   try {
